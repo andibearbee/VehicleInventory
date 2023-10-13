ab38a043b9c1f7ae77f8221eb6c6a7330fae2125
Andi <alexandra.e.burke@gmail.com>
2023-10-13 15:59:40 -0400

    VehicleInven First Steps

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Vehicle {
+    public static void main(String[] args) {
+        vehicleId - a long (ex: 101121);
+        makeModel - a string (ex: Ford Explorer);
+        color - a string (ex: Red);
+        odometerReading - an int (ex: 32775);
+        price - a float (ex: 12250.00) + (no Lamborghinis here!);
+
+    }
+
+}
---
+++ src/main/java/com/pluralsight/VehicleInventory.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class VehicleInventory {
+    public static void main(String[] args) {
+        String[] vehicles = new String[20];
+        for (int i = 0; i < vehicles.length; i++) {
+            System.out.print(vehicles[i] + " ");
+        }
+        System.out.println();
+    }
+}
